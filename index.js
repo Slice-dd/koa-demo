e983cdcf58b81f53d558012bc45cd0faec034740
slice <969567843@qq.com>
2018-08-26 22:54:45 +0800

    add comment

--- index.js
+++ index.js
@@ -5,36 +5,40 @@ const server = require('koa-static');
 const fs = require('fs');
 const path = require('path');
 
-const app = new Koa();
-const router = new Router()
+const app = new Koa(); // koa app
+const router = new Router(); // router
+
+app.use(server('.')); // static files and default redicft index.html
+
+// test async function
+// app.use(async (ctx, next) => {
+//     console.log(111) // 1
+//     await next();
+//     console.log(333); // 3
+// });
+// app.use((ctx, next) => {
+//     console.log(222); // 2
+// });
 
 
-
-app.use(async (ctx, next) => {
-    console.log(111)
-    await next();
-    console.log(333)
-})
-
-
-app.use(server('.'))
-console.log(router)
-
+// post request and use koabody to resolve request body
 router.post('/upload', koaBody({multipart: true}),  (ctx, next) => {
-    debugger
-    const data = ctx.request.body;
-    const files = ctx.request.files;
+
+    const data = ctx.request.body; // the request body
+    const files = ctx.request.files; // the request files
     
+    // response return to client
     ctx.body = {
         status: 1
-    }
+    };
     
-})
+});
 
 app
   .use(router.routes())
   .use(router.allowedMethods());
 
+// port listen
 app.listen(9000);
 
 
